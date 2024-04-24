4827cd4791b64e7941aa1bd001e96872be9096ce
Kaan <kaanasutay10@gmail.com>
2024-04-24 21:26:13 +0300

    viewRoutes ok

--- src/routes/view.js
+++ src/routes/view.js
@@ -5,13 +5,22 @@
 const router = require('express').Router()
 
 // Call Controllers:
-const {  BlogCategory: blogCategoryView ,BlogPost: blogPostView } = require('../controllers/blogControllerView')
+const { BlogCategory: blogCategoryView, BlogPost: blogPostView } = require('../controllers/blogControllerView')
+
+// router.all('/', blogPostView.list)
+// router.all('/create', blogPostView.create)
+// router.all('/:postId', blogPostView.read)
+// router.all('/:postId/update', blogPostView.update)
+// router.all('/:postId/delete', blogPostView.delete)
+
+router.all('/', (req, res) => {
+    res.redirect('/post')
+})
 
 router.all('/post', blogPostView.list)
 router.all('/post/create', blogPostView.create)
 router.all('/post/:postId', blogPostView.read)
 router.all('/post/:postId/update', blogPostView.update)
 router.all('/post/:postId/delete', blogPostView.delete)
-
 
 module.exports = router
