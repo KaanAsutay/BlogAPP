c9fcc4d3fd73c516cbd0591ec69adf0f4ac03534
Kaan <kaanasutay10@gmail.com>
2024-04-26 00:59:00 +0300

    TinyMCE static files

--- index.js
+++ index.js
@@ -47,6 +47,9 @@ app.use('/assets', express.static('./public/assets'))
 // Accept json data & convert to object:
 app.use(express.json())
 
+// TinyMCE static files:
+app.use('/tinymce', express.static('./node_modules/tinymce'))
+
 // Connect to MongoDB with Mongoose:
 require('./src/dbConnection')
 
